6b13ded4512f979d8e77cc00ed175a369177dc60
Eileen Mitchell <eileenhlmitchell19@gmail.com>
2021-12-09 20:30:24 -0800

    additions to JS code to start button click, create a key, attempt at connecting with data, failed.

--- script-org.js
+++ script-org.js
@@ -1,38 +1,59 @@
 //Print/Render the weather data to the page.
-var cityName = "q"
+var APP_ID = "2f5bc8626ea6c9333033abf0e40fcffb"
 
-//idk how to do this one -------------------------------------------
-var appid = `api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}`
-//------------------------------------------------------------------
+// Call this first: generatingPage();
 
-var timestamps = "cnt"
-
-var units = "imperial"
-//Fetched the geo data (lat, lon) 
-
-    // q = Name of the City
-
-    // limit = 5 (optional)
-
-    // appid = Your custom API key
-
-// Fetch the one call weather data
-//cityNameang
-function cityName(lat, lon) {
-    // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-
-    var url = `https://openweathermap.org/api?lat=${lat}&lon${lon}`;
+function populateCityForcast(cityName) {
+    let forcastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APP_ID;
+    
     // actually fetches url and turns it to a string using json
-    fetch( url )
-        .then(function(response) {
-            response.json();
-        })
-        // actually prints it to console
+    fetch(forcastUrl)  
+        .then(function(resp) { return resp.json() }) // Convert data to json
         .then(function(data) {
-            console.log( data );
+            var pickedForcastData = pickForcasts(data);
+            console.log("forcastData: " + pickedForcastData);
+            createCityForcastHtml(pickedForcastData);
         })
+        .catch(function(ex) {
+            console.log("Error" + ex);
+          // catch any errors
+        });
 }
+//create data using html
+function createCityForcastHtml(pickedForcastData) {
+    for(index=0;index<pickedForcastData.length();index++) {
+        var temprature = pickedForcastData[index].main.temp;
+        var humidity = pickedForcastData[index].main.humidity;
+        var windSpeed = pickedForcastData[index].wind.speed;
+        
+        // 1. Create Html for each day using above variables
+    }
+}
+//generating page and static html buttons
+function generatingPage() {
+    /*
+    <div class="card">
+            <h3 class="card-header text-uppercase">Search By City</h3>
+                <label class="form-label" for="username">Search by City</label>
+                <input name="cityTxtBxId" id="cityTxtBxId" type="text" autofocus="true"/>
+                <button id="searchBtnId" type="submit" class="btn">Search</button>
 
+    */
+
+    // 1. Get Search History from local Storage
+    //2. For all Cities in following loop
+    for(index=0;index<pickedForcastData.length();index++) {
+        /// 3. Create Buttons for each City e.g. <button class="cityBtnCls" type="submit" class="btn">Bothell</button>
+    }
+    // </div>
+    
+}
+//filterin g out to 5 correct responses. 12pm or 3pm?
+//-----------------------------------------------------------------
+function pickForcasts(forcastData) {
+    var pickedForcasts = forcastData.list.slice(0, 5);
+    return pickedForcasts;
+}
     // lat
 
     // lon
@@ -40,7 +61,7 @@ function cityName(lat, lon) {
     // units = imperial
 
     // exclude = minutely, hourly
-
+//--------------------------------------------------------------
     function oneCall(lat, lon) {
         var url = ``;
         // actually fetches url and turns it to a string using json
@@ -53,7 +74,7 @@ function cityName(lat, lon) {
                 console.log( data );
             })
     }
-    
+    //which api to use??? oncall not working not free?
         // lat
     
         // lon
@@ -74,10 +95,14 @@ function cityName(lat, lon) {
 
     // Get the city from the button's data attribute
 
-    $(".saveBtn").on("click", function(){
-        for(let i = 0; i < businessHours.length; i++){
-            let getTxtArea = $("#textarea" + i).val()
-            localStorage.setItem("textarea"+i, getTxtArea);
-        }
+    $("#searchBtnId").on("click", function() {
+        var cityNameFromUser = $("#cityTxtBxId").val();
+        populateCityForcast(cityNameFromUser);
+    })
+
+    $(".cityBtnCls").on("click", function(event) {
+        // 1. Add cityNameFromUser to Local Storage
+        // 2. Refresh the page
+        var cityNameFromUser = event.text();
     })
     //-------------------------------------------
